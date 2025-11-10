16a064a6123b409ac240b13de99d2c584876baba
bradkane <bkane@kaneconsulting.com>
2025-11-10 13:01:25 -0500

    nov2025 website;

---
+++ public/index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Bellariva Trattoria & Pizzeria</title>
+<meta name="AUTHOR" content="Bellariva Trattoria">
+<meta name="KEYWORDS" content="Restraunt, Italian Restraunt, Pizzaria, New Providence, NJ, 07974, Pizza, Summit, Berkeley Heights. Trattoria, Bellariva, Bellariva Pizzeria, Bellariva New Providence, Adams Shopping Center, Catering">
+<meta name="DESCRIPTION" content="Welcome to Bellariva Trattoria & Pizzeria located in New Providence, NJ!">
+
+<link rel="shortcut icon" href="favicon.ico">
+<style type="text/css">
+div#container
+{
+   width: 960px;
+   position: relative;
+   margin-top: 0px;
+   margin-left: auto;
+   margin-right: auto;
+   text-align: left;
+}
+body
+{
+   text-align: center;
+   margin: 0;
+}
+</style>
+<script type="text/javascript">
+<!--
+function PreloadImages()
+{
+   var imageObj = new Image();
+   var images = new Array();
+   images[0]="images/1.jpg";
+   images[1]="images/2.jpg";
+   images[2]="images/10.jpg";
+   images[3]="images/14.jpg";
+   images[4]="images/16.jpg";
+   for (var i=0; i<=4; i++)
+   {
+      imageObj.src = images[i];
+   }
+}
+// -->
+</script>
+<script language="JavaScript" type="text/javascript">
+<!--
+function FadeImage(id, duration)
+{
+   var millisec = Math.round(duration / 100);
+   var timer = 0;
+   for(i = 0; i <= 100; i++)
+   {
+      setTimeout("SetOpacity('" + id + "'," + i + ")",(timer * millisec));
+      timer++;
+   }
+}
+function SetOpacity(id, opacity)
+{
+   var element = document.getElementById(id).style;
+   element.opacity = (opacity / 100);
+   element.MozOpacity = (opacity / 100);
+   element.KhtmlOpacity = (opacity / 100);
+   element.filter = "alpha(opacity=" + opacity + ")";
+}
+//-->
+</script>
+<style type="text/css">
+a.Pizza_Menu:link {color: #FFFFFF;text-decoration: none;}
+a.Pizza_Menu:visited {color: #FFFFFF;text-decoration: none;}
+a.Pizza_Menu:active {color: #FFFFFF;text-decoration: none;}
+a.Pizza_Menu:hover {color: #7F7F7F;text-decoration: none;}
+</style>
+<style type="text/css">
+img { behavior: url("pngfix.html"); }
+</style>
+</head>
+<body background="preview_bkgrnd.png" bgcolor="#400000" text="#000000" ondragstart="return false;" ondrop="return false;">
+<div id="container">
+<div id="bv_Image8" style="overflow:hidden;position:absolute;left:1px;top:0px;z-index:0" align="left">
+<img src="images/banner.png" id="Image8" alt="" align="top" border="0" style="width:960px;height:219px;"></div>
+<div id="bv_Shape3" style="position:absolute;left:1px;top:227px;width:960px;height:531px;z-index:1;" align="center">
+<img src="images/bv01065.png" id="Shape3" align="top" alt="" title="" border="0" width="960" height="531"></div>
+<div id="bv_Shape4" style="position:absolute;left:0px;top:753px;width:962px;height:226px;z-index:2;" align="center">
+<img src="images/bv01076.png" id="Shape4" align="top" alt="" title="" border="0" width="962" height="226"></div>
+<div id="bv_Shape2" style="position:absolute;left:0px;top:217px;width:962px;height:60px;z-index:3;" align="center">
+<img src="images/bv01073.png" id="Shape2" align="top" alt="" title="" border="0" width="962" height="60"></div>
+<div id="bv_Text1" style="position:absolute;left:643px;top:81px;width:286px;height:96px;z-index:4;" align="right">
+<font style="font-size:27px" color="#000000" face="Arial"><b>TEL. 908-665-0033<br>
+TEL. 908-665-0327<br>
+FAX. 908-665-0304</b></font></div>
+<div id="bv_Html1" style="position:absolute;left:12px;top:766px;width:465px;height:200px;z-index:5;" align="left">
+<iframe width="465" height="200" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Bellariva+Pizzeria+%26+Restaurant,+Springfield+Avenue,+New+Providence,+NJ&amp;aq=0&amp;oq=Bellariva&amp;sll=40.07304,-74.724323&amp;sspn=4.648645,10.821533&amp;ie=UTF8&amp;hq=Bellariva+Pizzeria+%26+Restaurant,+Springfield+Avenue,+New+Providence,+NJ&amp;t=m&amp;ll=40.706636,-74.405637&amp;spn=0.005693,0.00912&amp;z=16&amp;output=embed&amp;iwloc=near"></iframe></div>
+<div id="bv_Shape5" style="position:absolute;left:490px;top:767px;width:459px;height:198px;opacity:0.50;-moz-opacity:0.50;-khtml-opacity:0.50;filter:alpha(opacity=50);z-index:6;" align="center">
+<img src="images/bv01077.gif" id="Shape5" align="top" alt="" title="" border="0" width="459" height="198"></div>
+<div id="bv_Table1" style="position:absolute;left:81px;top:220px;width:522px;height:46px;z-index:7;" align="left">
+<table width="100%" cellpadding="0" cellspacing="0" border="0" id="Table1">
+<tr>
+<td align="center" valign="middle" width="110" height="46"><font style="font-size:21px" color="#000000" face="Arial Black"><a href="../" target="_self" class="Pizza_Menu">Home</a></font></td>
+<td align="center" valign="middle" width="226" height="46"><font style="font-size:21px" color="#000000" face="Arial Black"><a href="../menu/index.html" target="_self" class="Pizza_Menu">Our Menu</a></font></td>
+<td align="center" valign="middle" width="186" height="46"><font style="font-size:21px" color="#000000" face="Arial Black"><a href="../gallery/index.html" target="_self" class="Pizza_Menu">Photo Gallery</a></font></td>
+</tr>
+</table></div>
+<div id="bv_Image4" style="overflow:hidden;position:absolute;left:779px;top:793px;z-index:8" align="left">
+<img src="images/logo150.png" id="Image4" alt="" align="top" border="0" style="width:158px;height:150px;"></div>
+<div id="bv_Text3" style="position:absolute;left:622px;top:879px;width:147px;height:57px;z-index:9;" align="left">
+<font style="font-size:16px" color="#000000" face="Arial"><b>TEL. 908-665-0033<br>
+TEL. 908-665-0327<br>
+FAX. 908-665-0304</b></font></div>
+<div id="bv_Image3" style="overflow:hidden;position:absolute;left:698px;top:228px;z-index:10" align="left">
+<a href="https://www.facebook.com/BellarivaTrattoria" target="_blank"><img src="images/facebook.png" id="Image3" alt="" align="top" border="0" style="width:32px;height:32px;"></a></div>
+<div id="bv_Image5" style="overflow:hidden;position:absolute;left:796px;top:228px;z-index:11" align="left">
+<a href="https://foursquare.com/v/bellariva-pizzeria/4c08235cbbc676b0291246d5" target="_blank"><img src="images/foursquare.png" id="Image5" alt="" align="top" border="0" style="width:32px;height:32px;"></a></div>
+<div id="bv_Image6" style="overflow:hidden;position:absolute;left:843px;top:228px;z-index:12" align="left">
+<a href="https://plus.google.com/115677713205008149681/about" target="_blank"><img src="images/google-plus.png" id="Image6" alt="" align="top" border="0" style="width:32px;height:32px;"></a></div>
+<div id="bv_Image7" style="overflow:hidden;position:absolute;left:747px;top:228px;z-index:13" align="left">
+<a href="https://www.yelp.com/biz/bellariva-trattoria-pizzeria-new-providence" target="_blank"><img src="images/yelp.png" id="Image7" alt="" align="top" border="0" style="width:32px;height:32px;"></a></div>
+<div id="bv_RotatingImage1" style="position:absolute;left:16px;top:362px;width:458px;height:343px;z-index:14;overflow:hidden" align="left">
+<script language="JavaScript" type="text/javascript">
+<!--
+   var RotatingImage1_Index = -1;
+   var RotatingImage1_Images = new Array();
+   RotatingImage1_Images[0] = ["images/1.jpg","",""];
+   RotatingImage1_Images[1] = ["images/2.jpg","",""];
+   RotatingImage1_Images[2] = ["images/10.jpg","",""];
+   RotatingImage1_Images[3] = ["images/14.jpg","",""];
+   RotatingImage1_Images[4] = ["images/16.jpg","",""];
+
+   function RotatingImage1ShowNext()
+   {
+      RotatingImage1_Index = RotatingImage1_Index + 1;
+      if (RotatingImage1_Index > 4)
+         RotatingImage1_Index = 0;
+      document.getElementById('RotatingImage1_Fade').src = document.getElementById('RotatingImage1').src;
+      SetOpacity('RotatingImage1', 0);
+      eval("document.RotatingImage1.src = RotatingImage1_Images[" + RotatingImage1_Index + "][0]");
+      setTimeout("RotatingImage1ShowNext();", 5000);
+      FadeImage('RotatingImage1', 1500);
+   }
+// -->
+</script>
+<img src="images/1.jpg" style="position:absolute;left:0px;top:0px;" id="RotatingImage1_Fade" border="0" align="top" alt="" width="458" height="343" name="RotatingImage1_Fade">
+<img src="images/1.jpg" style="position:absolute;left:0px;top:0px;" id="RotatingImage1" border="0" align="top" alt="" width="458" height="343" name="RotatingImage1">
+<script language="JavaScript" type="text/javascript">
+<!--
+  RotatingImage1ShowNext();
+// -->
+</script>
+</div>
+<div id="bv_Text4" style="position:absolute;left:496px;top:373px;width:433px;height:337px;z-index:15;" align="left">
+<font style="font-size:19px" color="#000000" face="Book Antiqua"><b>Bellariva Trattoria &amp; Pizzeria has been serving New Providence area customers since 1984. All of our food and sauces are made fresh daily with only the best ingredients to provide flavor beyond flavor every time.<br>
+<br>
+Bellariva offers dine-in, takeout, free delivery, and both on and off premise catering services. <br>
+<br>
+We accept all major credit cards. Please visit us or call to place an order.<br>
+<br>
+Open 7 Days a Week<br>
+10:30am-10:30pm<br>
+</b></font></div>
+<div id="bv_TextArt5" style="position:absolute;left:136px;top:290px;width:724px;height:48px;z-index:16;" align="center">
+<img src="images/bv01083.png" id="TextArt5" align="top" alt="" title="" border="0" width="724" height="48"></div>
+<div id="bv_Text6" style="position:absolute;left:491px;top:787px;width:270px;height:80px;z-index:17;" align="right">
+<font style="font-size:19px" color="#000000" face="Book Antiqua"><b>Bellariva Trattoria Pizzeria<br>
+</b></font><font style="font-size:15px" color="#000000" face="Book Antiqua"><b>Adams Shopping Center<br>
+1275 Springfield Avenue<br>
+New Providence, NJ 07974</b></font></div>
+<div id="bv_Image2" style="overflow:hidden;position:absolute;left:891px;top:228px;z-index:18" align="left">
+<a href="https://www.urbanspoon.com/r/53/580864/restaurant/North-Jersey/Summit/Bellariva-Trattoria-Pizzeria-New-Providence" target="_blank"><img src="images/bv01088.png" id="Image2" alt="" align="top" border="0" style="width:32px;height:32px;"></a></div>
+</div>
+</body>
+
+</html>
